259215658093bda51437d927549061e203f2ff51
Lindice <Lindicell@hotmail.com>
2020-11-05 17:37:19 -0300

    site

--- psite/assets/js/index.js
+++ psite/assets/js/index.js
@@ -1,82 +1,82 @@
-$(function() {
-    $('#WAButton').floatingWhatsApp({
-      phone: '1231231231', //WhatsApp Business phone number International format-
-      //Get it with Toky at https://toky.co/en/features/whatsapp.
-      headerTitle: 'Chat with us on WhatsApp!', //Popup Title
-      popupMessage: 'Hello, how can we help you?', //Popup Message
-      showPopup: true, //Enables popup display
-      buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
-      //headerColor: 'crimson', //Custom header color
-      //backgroundColor: 'crimson', //Custom background button color
-      position: "right"    
+/*----------------------------------------------------*/
+/* Mobile Navigation
+------------------------------------------------------ */
+var swiper = new Swiper('.swiper-container', {
+	speed: 600,
+	parallax: true,
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+	autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
+
+
+  
+
+  $(document).on('ready', function () {
+    // initialization of leaflet
+    $('#mapExample2').each(function () {
+      var leaflet = $.HSCore.components.HSLeaflet.init($(this)[0]);
+
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.AIzaSyCoRjby0GCtG6MGNY8kiaANy8UMfLeRbI8', {
+        id: 'mapbox/light-v9'
+      }).addTo(leaflet);
+    });
+
+    // initialization of form validation
+    $('.js-validate').each(function () {
+      var validation = $.HSCore.components.HSValidation.init($(this));
     });
   });
 
-var fadeStop = 300;
-$(document).ready(function(){
-  $('.navbar').css('background-color', 'rgba(0, 0, 0, 0)');
 
-  $('body').scrollspy({target: ".navbar", offset: 50});
-
-  // Add smooth scrolling on all links inside the navbar
-  $("#menu a").on('click', function(event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    }  // End if
-  });
+$('.mobileToggle').click(function() {
+    if ($('.mainNav').hasClass('openNav')) {
+        $('.mainNav').removeClass('openNav');
+    } else {
+        $('.mainNav').addClass('openNav');
+    }
 });
 
-$(document).scroll(function(){
-
-  var vScroll = $(this).scrollTop();
-  var opacity= 1-vScroll/fadeStop;
-  var visibility = vScroll/fadeStop;
-
-  $('.comp').css({
-    'opacity':''+opacity+'',
-    'transform':'translate('+-vScroll*2+'px, '+-vScroll/6+'px) scale('+opacity+')'
-  });
-
-  $('.navbar-brand').css('opacity', visibility);
-  $('.navbar').css('background-color', 'rgba(0, 0, 0, '+visibility/4+')');
-
-  if(vScroll > $('.box').offset().top-($(window).height()/1.2)){
-    $('.box').each(function(i){
-      setTimeout(function(){
-        $('.box').eq(i).addClass('trans');
-      },150*(i+1));
-    });
-  }
-
-  if(vScroll > $('.tab').offset().top-($(window).height()/1.2)){
-    $('.tab').each(function(i){
-      setTimeout(function(){
-        $('.tab').eq(i).addClass('flow');
-      },150*(i+1));
-    });
-  }
-
-  if(vScroll > $('.card').offset().top-($(window).height()/1.2)){
-    $('.card').each(function(i){
-      setTimeout(function(){
-        $('.card').eq(i).addClass('trans');
-      },150*(i+1));
-    });
-  }
-
+$('.mainNav li a').click(function() {
+    if ($('.mainNav').hasClass('openNav')) {
+        $('.navigation').removeClass('openNav');
+        $('.mainNav').removeClass('openNav');
+    }
 });
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+$(document).ready(function () {
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+});
+
+/*----------------------------------------------------*/
+/* Hero Animation
+------------------------------------------------------ */
+var hero = new TimelineMax({});
+	hero.staggerFrom(".first", 0.8, {opacity: 0}, 0.6)
+    .staggerFrom(".second", 0.6, {opacity: 0, y: 20}, 0.4, "-=0.2");
+    
+
+
+    
