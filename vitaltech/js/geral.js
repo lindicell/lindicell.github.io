eeef770ad13336b97cdc8e996107079863990d90
Líndice L. Leonardi <lindicell@hotmail.com>
2020-11-29 22:28:20 -0300

    Correções e loading

--- vitaltech/js/geral.js
+++ vitaltech/js/geral.js
@@ -2,6 +2,13 @@
 var $z = jQuery.noConflict();
 $z(document).ready(function(){
 	var contextFontResize = "html";
+	
+	var htmlFontSize = window.sessionStorage.getItem('htmlFontSize');
+	
+	if (htmlFontSize > 0) {
+		$z(contextFontResize).animate({'font-size' : htmlFontSize + 'px'});
+	}
+	
 	$z("#aumentar-fonte").click(function () {
 		var size = $z(contextFontResize).css('font-size');
 
@@ -9,6 +16,8 @@ $z(document).ready(function(){
 		size = parseInt(size) + 2;
 
 		$z(contextFontResize).animate({'font-size' : size + 'px'});
+		window.sessionStorage.setItem('htmlFontSize', size);
+		// $z(contextFontResize).css("fontSize", size);
 		return false;
 	});
 
@@ -19,6 +28,19 @@ $z(document).ready(function(){
 		size = parseInt(size) - 2;
 
 		$z(contextFontResize).animate({'font-size' : size + 'px'});
+		window.sessionStorage.setItem('htmlFontSize', size);
 		return false;
 	});
+	
+	setTimeout(() => {
+		loading(false);
+	}, 200);
 });
+
+function loading(status) {
+	if (status) {
+		$('.loading').show();
+	} else {
+		$('.loading').hide();
+	}
+}
